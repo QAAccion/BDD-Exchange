a4f943a0ec2dc038ae036529cf37813b11aa2ece
unknown <varun.agarwal@accionlabs.com>
2013-10-24 12:22:31 +0530

    Iteration#1

---
+++ Iteration#1/ConsoleApplication2/SpecFlowFeature1.feature
@@ -0,0 +1,16 @@
+﻿Feature:  health check
+
+    as a system administrator
+
+    I want a version health check
+
+    In order to know the Alteryx service is working
+
+
+   Scenario:  Active Districts
+
+   Given the alteryx service is running at "http://gallery.alteryx.com"
+
+   When I invoke the GET at "api/districts"
+
+   Then I see at least 6 active districts
---
+++ Iteration#1/HealthCheckSteps.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Web.Script.Serialization;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace DistrictCheck1
+{
+    [Binding]
+    public class DistrictCheck1Steps
+    {
+        private string theUrl;
+        private string _theResponse;
+
+        [Given(@"the alteryx service is running at ""(.*)""")]
+
+        public void GivenTheAlteryxServiceIsRunningAt(string p0)
+        {
+            theUrl = p0;
+        }
+
+        [When(@"I invoke the GET at ""(.*)""")]
+
+        public void WhenIInvokeTheGETAt(string p0)
+        {
+            string fullUrl = theUrl + "/" + p0;
+            WebRequest request = WebRequest.Create(fullUrl);
+            request.Credentials = CredentialCache.DefaultCredentials;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            Console.WriteLine(responseFromServer);
+            _theResponse = responseFromServer;
+        }
+
+        [Then(@"I see at least (.*) active districts")]
+
+        public void ThenISeeTheActiveDistricts(int expectedDistrict)
+        {
+            var json = new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<ArrayList>(_theResponse);
+            int count = json.Count;
+       
+            Assert.That(count, Is.AtLeast(expectedDistrict));
+        }
+    }
+}
